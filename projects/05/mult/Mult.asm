f8a84be1f8754ebeb63b0567bea152df47cfaf39
BrodyPennington <brodygpennington@gmail.com>
2024-10-25 22:02:37 -0500

    added Mult Documentation

--- projects/05/mult/Mult.asm
+++ projects/05/mult/Mult.asm
@@ -11,40 +11,62 @@
 
 // Put your code here.
 
-
+    // load R2
     @2
+    // set to 0
     M=0
     
+    // load R0
     @0
+    // D=R0
     D=M
+    // load END
     @END
+    //  if R0=0, jump to end
     D;JEQ
 
+    // load R1
     @1
+    // D=R1
     D=M
+    // load END
     @END
+    // if R1=0, jump to end
     D;JEQ
 
 (LOOP)
 
+    // select R0
     @0
+    // D=R0
     D=M
+    // load END
     @END
+    // when R0=0, jump to end
     D;JEQ
 
+    // load R1
     @1
+    // D=R1
     D=M
+    // load R2
     @2
+    // R2=R2+R1
     M=D+M
 
+    // load R0
     @0
+    // R0=R0-1
     M=M-1
-
+    
+    // load LOOP
     @LOOP
+    // unconditional jump to beginning of loop
     0;JMP
 
 (END)
-
+    // load END
     @END
+    // stay at end of program
     0;JMP
 
